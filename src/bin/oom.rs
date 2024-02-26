3083167cf75449a2bb25f702b5fea8fa864d7c25
Timur Guvenkaya <timur.guvenkaya@gmail.com>
2024-02-26 17:14:32 +0300

    makes code simpler

--- src/bin/oom.rs
+++ src/bin/oom.rs
@@ -1,9 +1,10 @@
 fn main() {
     let mut size: usize = 1024; // Start with 1KB
     while let Some(double_size) = size.checked_mul(2) {
-        match Vec::<usize>::with_capacity(double_size) {
-            _ => println!("Successfully allocated {} bytes", size),
-        }
+        let _ = Vec::<usize>::with_capacity(double_size);
+
+        println!("Successfully allocated {} bytes", size);
+
         size = double_size; // Double the allocation size on each iteration
     }
 
